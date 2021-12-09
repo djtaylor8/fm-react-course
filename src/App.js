88d93144633dedd50cc2ef4e30de38ad017e17e6
DJ <dtaylor88@gmail.com>
2021-12-08 19:02:05 -0800

    add props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement("div", {}, [
-        React.createElement("h2", {}, "Lou"),
-        React.createElement("h3", {}, "Cat"),
-        React.createElement("h3", {}, "Mega Chonk"),
+        React.createElement("h2", {}, props.name),
+        React.createElement("h3", {}, props.animal),
+        React.createElement("h3", {}, props.breed),
     ])
 }
 
@@ -12,9 +12,9 @@ const App = () => {
         {},
         [
           React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-          React.createElement(Pet),
-          React.createElement(Pet),
-          React.createElement(Pet),
+          React.createElement(Pet, { name: "Lou", animal: "Cat", breed: "MegaChonk"}),
+          React.createElement(Pet, { name: "Georgia", animal: "Cat", breed: "BestCat"}),
+          React.createElement(Pet, { name: "Sudo", animal: "Dog", breed: "Terrier"}),
         ]
     );
 };
