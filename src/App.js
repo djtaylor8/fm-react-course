41c4db7a0ae89425e3d2fc57688c156a7b10dfcb
DJ <dtaylor88@gmail.com>
2021-12-09 09:17:28 -0800

    add search form with basic usestate hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Lou" animal="Cat" breed="MegaChonk" />
-      <Pet name="Georgia" animal="Cat" breed="Siamese" />
-      <Pet name="Jackson" animal="Dog" breed="Labradoodle" />
+      <SearchParams />
     </div>
   );
 };
