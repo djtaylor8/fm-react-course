013dc39960586c1fd9677426765fe990198cc4b8
DJ <dtaylor88@gmail.com>
2021-12-09 13:51:09 -0800

    build carousel component class

---
+++ src/Carousel.js
@@ -0,0 +1,33 @@
+import { Component } from "react";
+
+class Carousel extends Component {
+  state = {
+    active: 0,
+  };
+
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {images.map((photo, idx) => (
+            <img
+              key={photo}
+              src={photo}
+              className={idx === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
