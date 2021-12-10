563f5b667d5589bfcbfff074f86a681209fe6615
DJ <dtaylor88@gmail.com>
2021-12-09 19:41:21 -0800

    lazy loading for code splitting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 /* eslint-disable import/named */
 /* eslint-disable import/namespace */
 import ReactDOM from "react-dom";
-import { StrictMode, useState } from "react";
+import { StrictMode, useState, lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import SearchParams from "./SearchParams";
-import Details from "./Details";
 import ThemeContext from "./ThemeContext";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   const theme = useState("darkblue");
@@ -19,21 +20,23 @@ const App = () => {
             "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
         }}
       >
-        <Router>
-          <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-400 to-red-500">
-            <Link to="/" className="text-6xl text-white hover:text-gray-200">
-              <h1>Adopt Me!</h1>
-            </Link>
-          </header>
-          <Switch>
-            <Route path="/details/:id">
-              <Details />
-            </Route>
-            <Route path="/">
-              <SearchParams />
-            </Route>
-          </Switch>
-        </Router>
+        <Suspense fallback={<h2>loading route</h2>}>
+          <Router>
+            <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-400 to-red-500">
+              <Link to="/" className="text-6xl text-white hover:text-gray-200">
+                <h1>Adopt Me!</h1>
+              </Link>
+            </header>
+            <Switch>
+              <Route path="/details/:id">
+                <Details />
+              </Route>
+              <Route path="/">
+                <SearchParams />
+              </Route>
+            </Switch>
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
